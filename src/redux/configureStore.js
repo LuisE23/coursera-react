1da40372a108a1216f2893a9e1f92d5b85b13515
Luis Ernesto Reynaldo Jordan <lreynaldoj@gmail.com>
2021-01-07 22:17:08 -0500

    first commit

---
+++ src/redux/configureStore.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Comments } from "./comments";
+import { Dishes } from "./dishes";
+import { Leaders } from "./leaders";
+import { Promotions } from "./promotions";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createForms } from "react-redux-form";
+import { InitialFeedback } from "./forms";
+
+export const ConfigureStore = () => {
+  const store = createStore(
+    combineReducers({
+      comments: Comments,
+      dishes: Dishes,
+      leaders: Leaders,
+      promotions: Promotions,
+      ...createForms({
+        feedback: InitialFeedback,
+      }),
+    }),
+    applyMiddleware(thunk, logger)
+  );
+
+  return store;
+};
